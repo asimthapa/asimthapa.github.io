4b213f665df3c8c834b4a6a28cf82bff586635fd
Asim Thapa <asimtut@gmail.com>
2019-07-28 14:53:42 -0400

    check

---
+++ scriptAbout.js
@@ -0,0 +1,10 @@
+var modal = document.getElementById('phoneModal');
+var closeBtn = document.getElemenstByClassName('closeBtn')[0];
+
+function popUpFunction() {
+   modal.style.display = 'block';
+}
+
+function closeFunction() {
+    modal.style.display = 'none';
+}
